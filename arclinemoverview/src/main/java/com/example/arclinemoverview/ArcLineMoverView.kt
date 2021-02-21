59527e99976f5e9cc193ee3f4fe5010500ee6aab
Anwesh43 <anweshthecool0@gmail.com>
2021-02-21 09:25:40 +0530

    adding constants and libraries

--- arclinemoverview/src/main/java/com/example/arclinemoverview/ArcLineMoverView.kt
+++ arclinemoverview/src/main/java/com/example/arclinemoverview/ArcLineMoverView.kt
@@ -1,2 +1,26 @@
 package com.example.arclinemoverview
 
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+
+val colors : Array<Int> = arrayOf(
+    "#F44336",
+    "#2196F3",
+    "#8BC34A",
+    "#009688",
+    "#FF9800"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 4
+val scGap : Float = 0.02f / parts
+val strokeFactor : Float = 90f
+val arcFactor : Float = 4.9f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
