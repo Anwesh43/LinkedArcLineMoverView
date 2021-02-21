1f27fb7b9ec188dd8df422eb9b479a3bdae6efe0
Anwesh43 <anweshthecool0@gmail.com>
2021-02-21 10:25:55 +0530

    adding ALMNode class

--- arclinemoverview/src/main/java/com/example/arclinemoverview/ArcLineMoverView.kt
+++ arclinemoverview/src/main/java/com/example/arclinemoverview/ArcLineMoverView.kt
@@ -119,4 +119,45 @@ class ArcLineMoverView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class ALMNode(var i : Int, val state : State = State()) {
+
+        private var next : ALMNode? = null
+        private var prev : ALMNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = ALMNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawALMNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : ALMNode {
+            var curr : ALMNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
