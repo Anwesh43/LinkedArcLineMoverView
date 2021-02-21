d956dc27ddaffd880908097f0dd393e66c235e17
Anwesh43 <anweshthecool0@gmail.com>
2021-02-21 09:41:41 +0530

    adding Canvas extension methods

--- arclinemoverview/src/main/java/com/example/arclinemoverview/ArcLineMoverView.kt
+++ arclinemoverview/src/main/java/com/example/arclinemoverview/ArcLineMoverView.kt
@@ -29,3 +29,30 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawArcLineMover(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / arcFactor
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, parts)
+    val sf2 : Float = sf.divideScale(1, parts)
+    val sf3 : Float = sf.divideScale(2, parts)
+    val sf4 : Float = sf.divideScale(3, parts)
+    save()
+    translate(size, h / 2)
+    save()
+    translate((w - size) * sf4, 0f)
+    drawArc(RectF(-size, -size, size, size), 0f, 360f * sf2, false, paint)
+    restore()
+    drawLine(-size * sf1, 0f, size * sf1, 0f, paint)
+    drawLine(0f, 0f, (w - size) * sf2, 0f, paint)
+    restore()
+}
+
+fun Canvas.drawALMNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawArcLineMover(scale, w, h, paint)
+}
